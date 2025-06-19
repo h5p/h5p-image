651c380298ac9f42e205291318b987a0885fb5da
William Almnes <william.almnes@gmail.com>
2025-06-19 08:41:09 +0200

    VA-785 add custom placeholder image and use H5P.Components (#76)
    * VA-785 add custom placeholder image and use H5P.Components
    
    * VA-785 fix scaling of placeholder and trigger loaded event

--- image.js
+++ image.js
@@ -8,6 +8,31 @@ var H5P = H5P || {};
  * @returns {undefined}
  */
 (function ($) {
+  const placeholderImg = `
+    <svg class="h5p-image-placeholder-svg" xmlns="http://www.w3.org/2000/svg" version="1.1" viewBox="0 0 903 459" preserveAspectRatio="xMidYMid slice">
+      <defs>
+        <style>
+          .cls-1 {fill: var(--h5p-theme-alternative-darker);}
+          .cls-2 {fill: var(--h5p-theme-alternative-dark);}
+          .cls-3 {fill: var(--h5p-theme-alternative-base);}
+        </style>
+      </defs>
+      <g>
+        <g id="Layer_1">
+          <rect class="cls-3" x="1" y="0" width="903.1" height="459.1"/>
+          <polygon class="cls-2" points="527.5 459.5 527.5 334.1 364.8 234 48.1 459.5 527.5 459.5"/>
+          <polygon class="cls-2" points="904.2 246 732.2 142.6 287.1 459.8 904.2 459.8 904.2 246"/>
+          <polygon class="cls-2" points="394.7 459.5 106.3 254 .1 332.4 .1 459.5 394.7 459.5"/>
+          <polygon class="cls-1" points="-.3 366.9 133.8 274.2 105.1 255.7 -.3 332.9 -.3 366.9"/>
+          <polygon class="cls-1" points="370.5 459.2 771.6 168.7 732.2 142.6 292 459.2 370.5 459.2"/>
+          <polygon class="cls-1" points="102.8 459.5 392.8 252.6 365.3 233.5 43.6 459.5 102.8 459.5"/>
+          <path class="cls-1" d="M43.3.4c30.1,78,105.7,133.3,194.3,133.3S401.8,78.3,431.8.4H43.3Z"/>
+          <path class="cls-2" d="M267.4.4L126.7,101.6c32.1,20.3,70.1,32,110.9,32,88.6,0,164.2-55.3,194.3-133.3h-164.5Z"/>
+        </g>
+      </g>
+    </svg>
+  `;
+
   H5P.Image = function (params, id, extras) {
     H5P.EventDispatcher.call(this);
     this.extras = extras;
@@ -45,17 +70,8 @@ var H5P = H5P || {};
 
     if (self.$img === undefined) {
       if(self.placeholder) {
-        self.$img = $('<div>', {
-          width: '100%',
-          height: '100%',
-          class: 'h5p-placeholder',
-          title: this.title === undefined ? '' : this.title,
-          on: {
-            load: function () {
-              self.trigger('loaded');
-            }
-          }
-        });
+        self.$img = $(H5P.Components.PlaceholderImg(placeholderImg));
+        self.trigger('loaded');
       } else {
         self.$img = $('<img>', {
           width: '100%',
